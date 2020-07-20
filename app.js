c315414307f317fb62fe4aaef77a718e20ae8451
kevin2HP <49846996+kevin2HP@users.noreply.github.com>
2020-07-20 10:37:52 +1000

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
-let posts = [];
 // Load the full build.
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -10,23 +10,42 @@ const ejs = require("ejs");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+deafultItems = [homeStartingContent, aboutContent, contactContent];
+
+mongoose.connect('mongodb://localhost:27017/BlogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+
+
+const Post = mongoose.model("Post", postSchema);
 
 
 app.get('/', function (req, res) {
-
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
-  });
+  Post.find({}, function (err, foundItems) {
+    if (err) {
+      console.log(err);
+      res.redirect('/')
+    } else {
+      res.render('home', {
+        homeStartingContent: homeStartingContent,
+        foundItems: foundItems
+      });
+    }
+  })
 });
 
 app.get('/about', function (req, res) {
@@ -51,32 +70,59 @@ app.post('/compose', function (req, res) {
   // console.log(postedItem);
   // title =req.body.postTitle;
   // content= req.body.postBody
-  const post = {
-    Title: req.body.postTitle,
+
+  const post = new Post({
+
+    title: req.body.postTitle,
+
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect('/')
+
+  });
+post.save();
+res.redirect('/')
 })
 
-
-
-app.get('/posts/:postName', function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function (post) { 
-    const storedTitle=_.lowerCase(post.Title);
-    const postContent =(post.content)
-    if (storedTitle === requestedTitle){
-      res.render('post',{storedTitle:post.Title,postContent:postContent});
-      console.log("Match Found!");
-
+app.post('/delete',function (req,res) {
+  const delId =req.body.del;
+  Post.findByIdAndRemove(delId,function (err) {
+    if (err){
+      console.log(err);
     }else{
-      console.log("Not Found");
+      console.log('Successfully deleted');
+      res.redirect('/');
+    }
+    })
+  
+  })
+
+// app.get('/posts/:postName', function (req, res) {
+//   const requestedTitle = _.lowerCase(req.params.postName);
+//   posts.forEach(function (post) {
+//     const storedTitle = _.lowerCase(post.Title);
+//     const postContent = (post.content)
+//     if (storedTitle === requestedTitle) {
+//       res.render('post', {
+//         storedTitle: post.Title,
+//         postContent: postContent
+//       });
+//       console.log("Match Found!");
+
+//     } else {
+//       console.log("Not Found");
+//     }
+//   })
+// });
+
+app.get('/posts/:postId', function (req, res) {
+  const requestedPostId = (req.params.postId);
+  Post.findOne({_id:requestedPostId},function (err,post) { 
+    if (err){
+    console.log(err);
+    }else{
+      res.render('post',{title:post.title,content:post.content})
     }
    })
-});
-
-
+})
 
 
 
